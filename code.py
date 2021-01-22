8d8e8f7c12a9b6df311abdaabfcc6b3d5a58f3e9
deepliya <50700950+deepliya@users.noreply.github.com>
2021-01-22 12:29:04 -0500

    Made changes  to to annoy player

--- code.py
+++ code.py
@@ -1,8 +1,8 @@
 def are_valid_groups(stdno, groups):
-    counter = 0
-    for i in groups:
+    counter = 1
+    for i in groups
         for j in range (len(stdno)):
-            if stdno[j] in i:
+            if stdno[i] in i:
                 counter = counter + 1
     if counter != len(stdno):
         return False
