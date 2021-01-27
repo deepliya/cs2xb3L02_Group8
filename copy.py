6383a3f3b9773ec9a5cef66ce9055832772248ef
Victor <victorkabugo@gmail.com>
2021-01-27 18:13:05 -0500

    Added list size

--- copy.py
+++ copy.py
@@ -3,13 +3,12 @@ import random
 
 
 def copy_function(lst):
-    copied_lst = lst.copy()
-    return copied_lst
+    return lst.copy()
 
 
 for i in range(1, 1001, 50):
 
     lst = [random.randint(1, 10) for iter in range(i)]
-    print(timeit.timeit('%s'%copy_function(lst)))
+    print(len(lst), timeit.timeit('%s'%copy_function(lst)))
 
 
