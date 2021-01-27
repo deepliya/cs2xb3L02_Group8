815dfd775825ce5c5200c20be2250334af384d6c
deepliya <50700950+deepliya@users.noreply.github.com>
2021-01-27 18:38:09 -0500

    changed code to automate lookup tests

--- lookup.py
+++ lookup.py
@@ -3,13 +3,9 @@ import random
 
 RandomListOfIntegers = [random.randint(1, 10) for iter in range(1000000)]
 
-def lookup_function_1():
-    value = 10
-    for i in RandomListOfIntegers:
-        if i == value:
-            return True
-    return False
+def look_up(value):
+    return RandomListOfIntegers[value]
 
-
-print(timeit.timeit(lookup_function_1))
+for i in range (1, 1000001, 1):
+    print (i, timeit.timeit('%s'%look_up(i))) 
 
