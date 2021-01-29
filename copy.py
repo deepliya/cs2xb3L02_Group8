2a5f2565f68c07dda29a59d0c3709b53cf0e07f2
Victor <victorkabugo@gmail.com>
2021-01-29 17:53:14 -0500

    Pulling code

--- copy.py
+++ copy.py
@@ -6,7 +6,7 @@ def copy_function(lst):
     return lst.copy()
 
 
-for i in range(1, 1001, 50):
+for i in range(0, 1001, 50):
 
     lst = [random.randint(1, 10) for iter in range(i)]
     print(len(lst), timeit.timeit('%s'%copy_function(lst)))
