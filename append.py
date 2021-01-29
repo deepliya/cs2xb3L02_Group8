5e26b9aaa5333e9c12c87b8009efb5e4c3c2237e
Victor <victorkabugo@gmail.com>
2021-01-29 17:51:41 -0500

    Changed code to time each append

--- append.py
+++ append.py
@@ -1,9 +1,5 @@
 import timeit
 
-def million_list():
-    lst = []
-    for i in range(1000000):
-        lst.append(i)
-    return lst
-
-print(timeit.timeit(million_list))
+lst = []
+for i in range(0, 1000001, 1000):
+    print(timeit.timeit('%s'%lst.append(i)))
