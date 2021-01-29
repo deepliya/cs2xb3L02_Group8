8573912c463095a66b34dc66deafefe539a31838
CozyChristopher <59571563+CozyChristopher@users.noreply.github.com>
2021-01-29 18:42:45 -0500

    changed lookup

--- lookup.py
+++ lookup.py
@@ -1,10 +1,10 @@
 import timeit
 import random
 
-RandomListOfIntegers = [random.randint(1, 10) for iter in range(1000001)]
+RandomListOfIntegers = [random.randint(1, 10) for iter in range(100001)]
 
 def look_up(value):
     return RandomListOfIntegers[value]
 
-for i in range (0, 10000001, 1000):
+for i in range (0, 100001):
     print (i, timeit.timeit('%s'%look_up(i)))
