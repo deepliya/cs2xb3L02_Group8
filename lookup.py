12104c39cf65bccceb95b129430a6aae076a0f48
Victor <victorkabugo@gmail.com>
2021-01-29 20:35:15 -0500

    Committed changes

--- lookup.py
+++ lookup.py
@@ -6,5 +6,5 @@ RandomListOfIntegers = [random.randint(1, 10) for iter in range(1000001)]
 def look_up(value):
     return RandomListOfIntegers[value]
 
-for i in range (0, 10000001, 1000):
-    print (i, timeit.timeit('%s'%look_up(i)))
+for i in range (0, 1000001,1000):
+    print (i)
