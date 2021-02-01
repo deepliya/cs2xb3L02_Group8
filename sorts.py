a3ebe0a0e3f2c1a03e1f430f1db23d8824df99ff
CozyChristopher <59571563+CozyChristopher@users.noreply.github.com>
2021-02-01 18:27:14 -0500

    added our_quicksort function

--- sorts.py
+++ sorts.py
@@ -1,7 +1,20 @@
 def quicksort_inplace(lst):
 	#should take a list as input and sort it without creating copies or other temporary lists
 
+def our_quicksorts(lst)
+
 def dual_pivot_quicksort(lst):
+    if len(L) < 2:
+        return L
+    pivot_1 = L[0]
+    pivot_2 = L[0]
+    left, right = [], []
+    for num in L[1:]:
+        if num < pivot:
+            left.append(num)
+        else:
+            right.append(num)
+    return quicksort_copy(left) + [pivot] + quicksort_copy(right)
 
 def tri_pivot_quicksort(lst):
 
