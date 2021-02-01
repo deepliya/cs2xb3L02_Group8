a19ed71931e83bd9fea8b65ac00ec88e06370df7
CozyChristopher <59571563+CozyChristopher@users.noreply.github.com>
2021-02-01 17:56:04 -0500

    added three functions

--- sorts.py
+++ sorts.py
@@ -1,2 +1,8 @@
 def quicksort_inplace(lst):
 	#should take a list as input and sort it without creating copies or other temporary lists
+
+def dual_pivot_quicksort(lst):
+
+def tri_pivot_quicksort(lst):
+
+def quad_pivot_quicksort(lst):
