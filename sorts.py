e77c3d82ccfdc391ea835fd273b2f8c6083ff968
CozyChristopher <59571563+CozyChristopher@users.noreply.github.com>
2021-02-01 18:58:39 -0500

    made dual pivot function

--- sorts.py
+++ sorts.py
@@ -1,20 +1,31 @@
+import random
+
 def quicksort_inplace(lst):
 	#should take a list as input and sort it without creating copies or other temporary lists
 
-def our_quicksorts(lst)
+def our_quicksorts(L):
+    copy = dual_pivot_quicksort(L)
+    for i in range(len(L)):
+        L[i] = copy[i]
+    print(L)
 
-def dual_pivot_quicksort(lst):
+
+def dual_pivot_quicksort(L):
     if len(L) < 2:
         return L
-    pivot_1 = L[0]
-    pivot_2 = L[0]
-    left, right = [], []
-    for num in L[1:]:
-        if num < pivot:
+    if L[0] > L[-1]:
+        L[0], L[-1] = L[-1], L[0]
+    pivot1 = L[0]
+    pivot2 = L[-1]
+    left, right, center = [], [], []
+    for num in L[1:-1]:
+        if num <= pivot1:
             left.append(num)
+        elif pivot1 < num < pivot2:
+            center.append(num)
         else:
             right.append(num)
-    return quicksort_copy(left) + [pivot] + quicksort_copy(right)
+    return dual_pivot_quicksort(left) + [pivot1] + dual_pivot_quicksort(center) + [pivot2] + dual_pivot_quicksort(right)
 
 def tri_pivot_quicksort(lst):
 
