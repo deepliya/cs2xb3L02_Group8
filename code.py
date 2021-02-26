ef3b7efe45302762b3c11346dad5e3512a80f9fa
deepliya <50700950+deepliya@users.noreply.github.com>
2021-02-25 19:57:50 -0500

    Split time_test into 3 tests

--- code.py
+++ code.py
@@ -5,11 +5,8 @@ from heap.py import *
 def create_random_list(n):
     return [random.random() for _ in range(n)]
 
-def time_test(n, runs):
+def time_test1(n, runs):
     total1 = 0
-    total2 = 0
-    total3 = 0
-
     for _ in range(runs):
         L = create_random_list(n)
         heap1 = Heap(L)
@@ -17,7 +14,10 @@ def time_test(n, runs):
         heap1.build_heap1()
         total1 += timeit.default_timer() - start
     runtime1 = total1 / runs
+    return runtime1
 
+def time_test2(n, runs):
+    total2 = 0
     for _ in range(runs):
         L = create_random_list(n)
         heap1 = Heap(L)
@@ -25,7 +25,10 @@ def time_test(n, runs):
         heap1.build_heap2()
         total2 += timeit.default_timer() - start
     runtime2 = total2 / runs
+    return runtime2
 
+def time_test3(n, runs):
+    total3 = 0
     for _ in range(runs):
         L = create_random_list(n)
         heap1 = Heap(L)
@@ -33,8 +36,7 @@ def time_test(n, runs):
         heap1.build_heap3()
         total3 += timeit.default_timer() - start
     runtime3 = total3 / runs
-
-    return runtime1, runtime2, runtime3
+    return runtime3
 
 for i in range(1, 1001):
-    print(time_test(i, 20))
+    print(i, time_test1(i, 20), time_test2(i, 20), time_test3(i, 20) )
