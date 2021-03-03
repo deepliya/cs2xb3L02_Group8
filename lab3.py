aa0d23801bf867d2e7268a510fa7624f00314f11
Victor <victorkabugo@gmail.com>
2021-03-03 18:10:33 -0500

    Fixed merge conflicts

--- lab3.py
+++ lab3.py
@@ -1,5 +1,6 @@
 import random
 import math
+import timeit
 
 def my_quicksort(L):
     copy = quicksort_copy(L)
@@ -36,3 +37,28 @@ def create_near_sorted_list(n, factor):
         L[index1], L[index2] = L[index2], L[index1]
     return L
 
+def dual_pivot_quicksort(L):
+    if len(L) < 2:
+        return L
+
+    #put the pivots in ascending order
+    if L[0] > L[-1]:
+        L[0], L[-1] = L[-1], L[0]
+
+    pivot1 = L[0]
+    pivot2 = L[-1]
+    left, right, center = [], [], []
+
+    for num in L[1:-1]:
+        if num <= pivot1:
+            left.append(num)
+        elif pivot1 < num < pivot2:
+            center.append(num)
+        else:
+            right.append(num)
+    return dual_pivot_quicksort(left) + [pivot1] + dual_pivot_quicksort(center) + [pivot2] + dual_pivot_quicksort(right)
+
+for n in range(0, 200 + 1):
+
+    list = create_random_list(n)
+    print(timeit.timeit('%s' % dual_pivot_quicksort(list)))
