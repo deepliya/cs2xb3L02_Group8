532e3c9b97132d1aed4fd8ee4ed3ed345640d4e3
Victor <victorkabugo@gmail.com>
2021-03-12 18:55:53 -0500

    Added random sample function

--- code.py
+++ code.py
@@ -5,14 +5,16 @@ import random
 def cycle_test(n, e):
     g = Graph(n + 1)
     for i in range (1, e + 1):
-        g.add_edge(random.randint(1, 101), random.randint(1, 101))
+        nodes = random.sample(range(1, 101), 2)
+        g.add_edge(nodes[0], nodes[1])
     print(has_cycle(g))
     return has_cycle(g)
 
 def is_connected_test(n, e):
     g = Graph(n + 1)
     for i in range (1, e + 1):
-        g.add_edge(random.randint(1, 101), random.randint(1, 101))
+        nodes = random.sample(range(1, 101), 2)
+        g.add_edge(nodes[0], nodes[1])
     print(is_connected(g))
     return is_connected(g)
 
