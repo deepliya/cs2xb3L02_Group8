c1a8ee8a40ea8cd1e997f3f6868cffc7d486a3cc
Victor <victorkabugo@gmail.com>
2021-03-12 22:19:58 -0500

    Changed test code

--- code.py
+++ code.py
@@ -24,7 +24,7 @@ def cycle_test2(runs, k, c):
 
         counter = 0
 
-        for i in range(runs):
+        for j in range(runs):
 
             G = Graph(k)
 
@@ -33,27 +33,46 @@ def cycle_test2(runs, k, c):
 
             if has_cycle(G):
 
-                counter += 1
-
-        print(counter, " ", c)
-
+                counter = counter + 1
+        print(counter, " ", i)
 
 def connected_test2(runs, k, c):
+
     for i in range(c):
 
         counter = 0
 
-        for i in range(runs):
+        for j in range(runs):
 
             G = Graph(k)
 
-            nodes = random.sample(range(0, n), 2)
+            nodes = random.sample(range(0, k), 2)
             G.add_edge(nodes[0], nodes[1])
 
             if is_connected(G):
-                counter += 1
 
-        print(counter, " ", c)
+                counter = counter + 1
+
+        print(counter, " ", i)
 
 
+def connected_test3(runs, k, c):
+    for i in range(c):
 
+        counter = 0
+
+        for j in range(runs):
+
+            G = Graph(k)
+
+            for b in range(c):
+
+                nodes = random.sample(range(0, k), 2)
+                G.add_edge(nodes[0], nodes[1])
+
+            if is_connected(G):
+                counter = counter + 1
+
+        print(counter, " ", i)
+
+connected_test2(100, 100, 5000)
