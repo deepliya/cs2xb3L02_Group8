8e67f8a125822a798cc33746fb44bae847ce786b
Victor <victorkabugo@gmail.com>
2021-03-12 19:27:12 -0500

    Added testing code

--- code.py
+++ code.py
@@ -1,21 +1,59 @@
 from graphs import *
 import random 
 
-#Testing functions
+#Testing Functions
 def cycle_test(n, e):
-    g = Graph(n + 1)
-    for i in range (1, e + 1):
-        nodes = random.sample(range(1, 101), 2)
+    g = Graph(n)
+    for i in range(e):
+        nodes = random.sample(range(0, n), 2)
         g.add_edge(nodes[0], nodes[1])
     print(has_cycle(g))
     return has_cycle(g)
 
 def is_connected_test(n, e):
-    g = Graph(n + 1)
-    for i in range (1, e + 1):
-        nodes = random.sample(range(1, 101), 2)
+    g = Graph(n)
+    for i in range(e):
+        nodes = random.sample(range(0, n), 2)
         g.add_edge(nodes[0], nodes[1])
     print(is_connected(g))
     return is_connected(g)
 
-is_connected_test(100, 30)
+def cycle_test2(runs, k, c):
+
+    for i in range(c):
+
+        counter = 0
+
+        for i in range(runs):
+
+            G = Graph(k)
+
+            nodes = random.sample(range(0, n), 2)
+            G.add_edge(nodes[0], nodes[1])
+
+            if has_cycle(G):
+
+                counter += 1
+
+        print(counter, " ", c)
+
+
+def connected_test2(runs, k, c):
+    for i in range(c):
+
+        counter = 0
+
+        for i in range(runs):
+
+            G = Graph(k)
+
+            nodes = random.sample(range(0, n), 2)
+            G.add_edge(nodes[0], nodes[1])
+
+            if is_connected(G):
+                counter += 1
+
+        print(counter, " ", c)
+
+
+
