edfad53acb025b2920fc4a29b4949b88206c4788
deepliya <50700950+deepliya@users.noreply.github.com>
2021-03-12 23:32:59 -0500

    Added test1

--- code.py
+++ code.py
@@ -2,6 +2,14 @@ from graphs import *
 import random 
 
 #Testing Functions
+
+def cycle_test(n, e):
+    g = Graph(n)
+    for i in range(e):
+        nodes = random.sample(range(0, n), 2)
+        g.add_edge(nodes[0], nodes[1])
+    return has_cycle(g)
+
 def is_connected_test(n, e):
     g = Graph(n)
     for i in range(e):
@@ -9,13 +17,20 @@ def is_connected_test(n, e):
         g.add_edge(nodes[0], nodes[1])
     return is_connected(g)
 
+def test1():
+    n = 100
+    for c in range (200):
+        counter = 0
+        for i in range(50):
+            if cycle_test(n, c):
+                counter += 1
+        print(c, counter)
+
 def test2():
     n = 100
     for c in range (500):
         counter = 0
-        for i in range(50):
-            if is_connected_test(n,c):
+        for i in range(20):
+            if is_connected_test(n, c):
                 counter += 1
-        print(counter, " ", c)
-
-test2()
+        print(c, counter)
