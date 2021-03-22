6b35eee549613307883a89d5c215785c02b0d84e
Victor <victorkabugo@gmail.com>
2021-03-22 19:41:26 -0400

    Committed changes before pull

---
+++ mst.py
@@ -0,0 +1,18 @@
+import min_heap
+import lab8
+
+def prim1():
+
+def prim2(G):
+
+    L = []
+
+    for i in range(G.number_of_nodes):
+        if i != 0:
+            L[i] = 99999
+
+    min_heap = MinHeap(L)
+
+    while G.
+
+
