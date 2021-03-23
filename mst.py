6e56c40db32c11588bf2edc05c6c5f787a3bb0d2
CozyChristopher <59571563+CozyChristopher@users.noreply.github.com>
2021-03-23 19:51:54 -0400

    updated prim1

--- mst.py
+++ mst.py
@@ -1,4 +1,5 @@
 from lab8 import *
+import copy
 
 def prim2(g):
 
@@ -37,32 +38,31 @@ def prim2(g):
         MST.add_edge(min_key,minNode,g.)
 
 def prim1(g):
-
     global node2, node1, node_edge_combo
-    MST = WeightedGraph(g.number_of_nodes())
-
+    orig_graph = copy.deepcopy(g)
+    MST = WeightedGraph(orig_graph.number_of_nodes())
     #Inital set A with two nodes and one edge
-    A = [list(g.adj.keys())[0]]
-    smallest_edge = g.adj[0][0][1]
-    for i in g.adj[0]:
+    A = [list(orig_graph.adj.keys())[0]]
+    smallest_edge = orig_graph.adj[0][0][1]
+    for i in orig_graph.adj[0]:
         if i[1] < smallest_edge:
             smallest_edge = i[1]
             A.append(i[0])
-            MST.add_edge(i[0], list(g.adj.keys())[0], smallest_edge)
-            g.adj[0].remove(i)
-            for j in g.adj[i[0]]:
+            MST.add_edge(i[0], list(orig_graph.adj.keys())[0], smallest_edge)
+            orig_graph.adj[0].remove(i)
+            for j in orig_graph.adj[i[0]]:
                 if j[0] == 0:
-                    g.adj[i[0]].remove(j)
+                    orig_graph.adj[i[0]].remove(j)
                     break
 
-    while len(A) < g.number_of_nodes():
+    while len(A) < orig_graph.number_of_nodes():
         #iterate thru all the nodes in A
         smallest_edge = 1000
         for node in A:
             #find smallest edge of all that isn't connected to a node already in A
             #add the node to A
             #add edge to MST
-            for i in g.adj[node]:
+            for i in orig_graph.adj[node]:
                 if i[1] <= smallest_edge and i[0] not in A:
                     smallest_edge = i[1]
                     node_edge_combo = i
@@ -70,10 +70,10 @@ def prim1(g):
                     node1 = node
         A.append(node2)
         MST.add_edge(node1, node2, smallest_edge)
-        g.adj[node1].remove(node_edge_combo)
-        for j in g.adj[node_edge_combo[0]]:
+        orig_graph.adj[node1].remove(node_edge_combo)
+        for j in orig_graph.adj[node_edge_combo[0]]:
             if j[0] == node1:
-                g.adj[node_edge_combo[0]].remove(j)
+                orig_graph.adj[node_edge_combo[0]].remove(j)
                 break
-                
+
     return MST.adj
